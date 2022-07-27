66671ddb2056036e27e4b1e64151eea7d9dfc3e3
cecell <91251956+celaacl@users.noreply.github.com>
2022-07-27 09:14:35 +0700

    tambah file

---
+++ Boss.java
@@ -0,0 +1,24 @@
+package ukltralala;
+
+public class Boss {
+    private int gaji;
+    private String NIP;
+    public String Nama;
+
+    public Boss () {
+    this.gaji  = 10000000;
+    this.NIP  = "1234567890" ;
+    this.Nama  = "cece";
+
+
+    }
+
+    public int getGaji() {
+        return this.gaji;
+
+    }
+    public String getNip() {
+        return this.NIP;
+    }
+
+}
